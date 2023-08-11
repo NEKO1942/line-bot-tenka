ca77b4103f0f1ae56f0fa3b3845dac373132d79c
BuildTools <141541155+NEKO1942@users.noreply.github.com>
2023-08-12 00:28:00 +0900

    Create youtube_channel.py

---
+++ youtube_channel.py
@@ -0,0 +1,24 @@
+channel_name = {
+  "k4sen": "UChUjERVG8eKEAJSCef-tTmg",
+  "かせん": "UChUjERVG8eKEAJSCef-tTmg",
+  "かせんさん": "UChUjERVG8eKEAJSCef-tTmg",
+  "k4senさん": "UChUjERVG8eKEAJSCef-tTmg",
+  "ニキ": "UChUjERVG8eKEAJSCef-tTmg",
+  "釈迦": "UCjg5-lUiUzdmzzBRY9Xqrhw",
+  "釈迦さん": "UCjg5-lUiUzdmzzBRY9Xqrhw",
+  "しゃか": "UCjg5-lUiUzdmzzBRY9Xqrhw",
+  "しゃかさん": "UCjg5-lUiUzdmzzBRY9Xqrhw",
+  "shaka": "UCjg5-lUiUzdmzzBRY9Xqrhw",
+  "shakaさん": "UCjg5-lUiUzdmzzBRY9Xqrhw",
+  "関優太": "UC2j4lymo8Ce_1RXMeo4afcQ",
+  "関さん": "UC2j4lymo8Ce_1RXMeo4afcQ",
+  "スタヌ": "UC2j4lymo8Ce_1RXMeo4afcQ",
+}
+
+
+
+support_channel ={
+    "k4sen",
+    "釈迦",
+    "関優太",
+}
